4dc2d6c72c7a3e308c510417dda32b1ecd0c43f2
DuDu Silva <raimundoeduardo.silva@gmail.com>
2022-05-21 13:47:13 -0300

    exemplos de variaveis

---
+++ sintaxe-variaveis-fluxo/src/main/java/TestaVariaveis.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Eduardo Silva
+ */
+public class TestaVariaveis {
+    public static void main(String[] args){
+        System.out.println("Variaveis");
+        int idade;
+        idade = 18;
+        idade = idade + 10+(2-1); 
+        System.out.println("int idade - "+idade);
+        
+        double peso = 3.14;
+        System.out.println("double peso - " + peso);
+        
+        char caracter = 'E';
+        System.out.println("char caracter - " + caracter);
+        
+        String nome = "Eduardo";
+        System.out.println("String nome - " + nome);
+    }
+}
