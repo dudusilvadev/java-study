001a02b5c5db31b5c1ca8d01ebd962f8f7075b7d
DuDu Silva <raimundoeduardo.silva@gmail.com>
2022-05-21 13:48:44 -0300

    exemplos de variaveis

---
+++ sintaxe-variaveis-fluxo/src/main/java/DesafioFatorial.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Eduardo Silva
+ */
+public class DesafioFatorial {
+    public static void main(String[] args) {
+        int fatorial = 1;
+            for (int i = 1; i < 11; i++) {
+                fatorial *= i;
+                System.out.println("Fatorial de " + i + " = " + fatorial);
+                
+            
+            
+        }
+    }
+    
+}
---
+++ sintaxe-variaveis-fluxo/src/main/java/MultiploTres.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Eduardo Silva
+ */
+public class MultiploTres {
+    public static void main(String[] args) {
+                 for (int i = 0; i < 100; i++) {
+                     
+                     if (i % 3 == 0) {
+                         
+                      System.out.println(i);
+                         
+                     }
+            
+        }
+                  
+            
+        }
+    }
+
